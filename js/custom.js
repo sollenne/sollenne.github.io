82d711fc5d6356596d089d48fcedb9a067ed1139
sollenne <rich@sollenne.com>
2016-02-01 20:18:10 -0600

    cool header start

--- js/custom.js
+++ js/custom.js
@@ -3,4 +3,21 @@ $(document).ready(function() {
 	// fitText
 	$(".fittext").fitText();
 
+    // animate header
+    var _header = $('header');
+    $(function(){
+        $(window).scroll(function() {
+            var scroll = getCurrentScroll();
+            if (scroll >= _header.height()) {
+                _header.addClass('shrink');
+            }
+            else {
+                _header.removeClass('shrink');
+            }
+        });
+        function getCurrentScroll() {
+            return window.pageYOffset || document.documentElement.scrollTop;
+        }
+    });
+
 });
