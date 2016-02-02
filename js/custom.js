40dfbd3aea894823d45a482b83d0c2777afba5a8
sollenne <rich@sollenne.com>
2016-02-02 14:21:11 -0600

    changes regarding active classes and footer links

--- js/custom.js
+++ js/custom.js
@@ -27,19 +27,44 @@ $(document).ready(function() {
 
     // Custom Scroll to div on Home Page
     $(function() {
-        $("a[href*='#']:not([href='#'])").click(function() {
+        var _navClicked = $("a[href*='#']:not([href='#'])");
+        _navClicked.click(function() {
             if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
                 var target = $(this.hash);
                 target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
                 if (target.length) {
+                    $(this).addClass('almost-active');
                     $('html,body').animate({
                         scrollTop: target.offset().top
-                    }, 750);
+                    }, 750, function() {
+                        _navClicked.removeClass('almost-active');
+                    });
                     return false;
                 }
             }
         });
     });
+
+    // Active Nav on Scroll
+    var _sections = $('section')
+        , _header_height = _header.outerHeight();
+    $(window).on('scroll', function () {
+        var cur_pos = $(this).scrollTop();
+        _sections.each(function() {
+            var top = $(this).offset().top - _header_height,
+                bottom = top + $(this).outerHeight();
+
+            if (cur_pos >= top && cur_pos <= bottom) {
+                _header.find('a').removeClass('active');
+                _sections.removeClass('active');
+
+                $(this).addClass('active');
+                _header.find('a[href="#'+$(this).attr('id')+'"]').addClass('active');
+            }
+        });
+    });
+
+
 
     // Parallax on Home Page
     skrollr.init({
